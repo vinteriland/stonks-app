aafb36f95b18e199be9a3292cb9fc705e6fe7257
Tript Dhudi <52734292+vinteriland@users.noreply.github.com>
2024-05-29 12:40:15 -0400

    Environment and Dependency Setup

---
+++ stonks-app/utils/supabaseClient.js
@@ -0,0 +1,20 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+export const signInWithGoogle = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'google',
+    });
+    if (error) console.error('Error signing in:', error);
+    return { user, session, error };
+  };
+  
+  export const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.error('Error signing out:', error);
+  };
+  
