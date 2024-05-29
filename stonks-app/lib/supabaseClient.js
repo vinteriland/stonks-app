7c034f3162dc83fa16a51595fab99f7543c36146
Tript Dhudi <52734292+vinteriland@users.noreply.github.com>
2024-05-29 10:33:47 -0400

    Google Authentication Setup

--- stonks-app/lib/supabaseClient.js
+++ stonks-app/lib/supabaseClient.js
@@ -4,3 +4,17 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const signInWithGoogle = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'google',
+    });
+    if (error) console.error('Error signing in:', error);
+    return { user, session, error };
+  };
+  
+  export const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.error('Error signing out:', error);
+  };
+  
