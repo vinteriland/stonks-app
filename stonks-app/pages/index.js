6fe1b8e54e50ff1351eb0962056e044e285e8e11
Tript Dhudi <52734292+vinteriland@users.noreply.github.com>
2024-05-29 12:41:50 -0400

    node upgrade

---
+++ stonks-app/pages/index.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { supabase } from '../lib/supabaseClient';
+
+export default function Home() {
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    const fetchChannels = async () => {
+      const { data } = await supabase.from('profiles').select('*');
+      setChannels(data);
+    };
+    fetchChannels();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1>Channels</h1>
+      <ul>
+        {channels.map((channel) => (
+          <li key={channel.id}>
+            <Link href={`/channel/${channel.id}`}>
+              <a>{channel.username}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
